0ed6906a1ce2c68af27239a827dd0f8a31f5d0e8
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-05-16 13:20:49 +0200

    creating checkbutton event listener

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 'use strict';
 
 // grabbing DOM elements
-const message = document.querySelector('.message').textContent;
+const message = document.querySelector('.message');
 const number = document.querySelector('.number');
-const score = document.querySelector('.score').textContent;
-const guessInput = document.querySelector('.guess').value;
+const guessInput = document.querySelector('.guess');
+const btnCheck = document.querySelector('.check');
+let scoreElement = document.querySelector('.score');
+
+// Generating random number
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+
+// Declaring score variable to count down each attempt
+let score = 20;
+
+// Adding even listener to check button and display message according user input
+btnCheck.addEventListener('click', function() {
+
+    const guess = Number(guessInput.value);
+
+    if(!guess) {
+        message.innerHTML = `🤦‍♀️ No Number`;
+    } else if (guess === secretNumber) {
+        message.innerHTML = `👍👏 Correct Number!`;
+    } else if (guess < secretNumber) {
+        message.innerHTML = `Number to LOW!`
+        score--;
+        scoreElement.innerHTML = score;
+    } else if (guess > secretNumber) {
+        message.innerHTML = `Number to HIGH!`
+        score--;
+        scoreElement.innerHTML = score;
+    }
+})
 
 
