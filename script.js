760d45cf03d91fe367478435ac5e697ae9935cd2
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-05-16 12:23:12 +0200

    grabbing DOM elements

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 'use strict';
+
 // grabbing DOM elements
-const message = document.querySelector('.message');
+const message = document.querySelector('.message').textContent;
+const number = document.querySelector('.number');
+const score = document.querySelector('.score').textContent;
+const guessInput = document.querySelector('.guess').value;
+
+
